5b2c69e86d49aa7f65a0010f1d987f984cab6e83
hai.cony <54712756+hai-cony@users.noreply.github.com>
2023-10-11 19:12:23 +0700

    Update http request to https request

--- ConyHomeAutomation.cpp
+++ ConyHomeAutomation.cpp
@@ -2,6 +2,7 @@
 #include <Arduino.h>
 #include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
+#include <WiFiClientSecure.h>
 #include <WiFiClient.h>
 #include <EEPROM.h>
 
@@ -9,108 +10,129 @@
 
 #include <ArduinoJson.h>
 
-
 String jsonWebToken;
 unsigned long lastTime = 0;
 
-/* 
+const uint8_t fingerprint[20] = {0xbb, 0x8b, 0x2c, 0xd9, 0x45, 0x95, 0xd5, 0xd8, 0xfc, 0x8a, 0x8d, 0x67, 0x56, 0x71, 0xd5, 0x99, 0xad, 0xf6, 0xd5, 0xfe};
+
+/*
   return the token from JsonWebToken. Function are not allowed
-  use SSID and Password of WiFi user dynamically. Set SSID and Password 
+  use SSID and Password of WiFi user dynamically. Set SSID and Password
   as parameter. ex: startConnectionAndGetToken("my_wifi_ssid", "my_wifi_password");
 */
-String ConyHomeAutomation::startConnectionAndGetToken(String ssid, String password){
-    if (WiFi.status() != WL_CONNECTED){
-      Serial.printf("SSID : %s\n", ssid);
-      Serial.printf("Password : %s\n", password);
-      Serial.println("Connecting");
-      WiFi.begin(ssid, password);
-      while(WiFi.status() != WL_CONNECTED) {
-        delay(500);
-        Serial.print(".");
-      }
-      Serial.println("");
-      Serial.print("Connected to WiFi network with IP Address: ");
-      Serial.println(WiFi.localIP());
+String ConyHomeAutomation::startConnectionAndGetToken(String ssid, String password)
+{
+  // WiFiClientSecure client;  
+  
+  if (WiFi.status() != WL_CONNECTED){
+    Serial.printf("SSID : %s\n", ssid);
+    Serial.printf("Password : %s\n", password);
+    Serial.println("Connecting");
 
-      for(uint8_t t = 3; t > 0; t--) {
-          Serial.printf("[SETUP] BOOT WAIT %d...\n", t);
-          delay(1000);
-      }
+    WiFi.begin(ssid, password);
+
+    while (WiFi.status() != WL_CONNECTED){
+      delay(500);
+      Serial.print(".");
     }
-    
 
-    // Initialization, request the token
-    WiFiClient client;
-    HTTPClient http;
-    // Your Domain name with URL path or IP address with path
-    http.begin(client, "http://192.168.100.2:8080/api/esp8266/auth");
-    http.addHeader("Content-Type", "application/json");
+    Serial.println("");
+    Serial.println(WiFi.macAddress());
+    Serial.println(String(ESP.getChipId()));
+    Serial.print("Connected to WiFi network with IP Address: ");
+    Serial.println(WiFi.localIP());
 
-    // JSON Declare
-    DynamicJsonDocument doc(2048);
-    
-    doc["chip_id"] = String(ESP.getChipId());
-    doc["mac"] = WiFi.macAddress();
-    doc["api_key"] = "$argon2id$v=19$m=65536,t=3,p=4$gX6VFv3fKlvvqkvHBbpxWw$XVqT4tzrOjNj/oSoHx2qb5IG6TYB8mNM/SYdWQXHS+g";
+    for (uint8_t t = 3; t > 0; t--)
+    {
+      Serial.printf("[SETUP] BOOT WAIT %d...\n", t);
+      delay(1000);
+    }
+  }
 
-    // Serialize JSON document
-    String json;
-    serializeJson(doc, json);
-      
-    // Send HTTP POST request
-    int httpResponseCode = http.POST(json);
-    if (httpResponseCode == 200) {                                     
-      String payload = http.getString();
-      StaticJsonDocument<512> jsonBuffer;
-      deserializeJson(doc, payload);
-      String newToken = doc["token"];
-      jsonWebToken = newToken;
-    } 
-    http.end();
-    return jsonWebToken; // return the jwt token value only
+  std::unique_ptr<BearSSL::WiFiClientSecure>client(new BearSSL::WiFiClientSecure);
+
+  client->setFingerprint(fingerprint);
+
+  HTTPClient https;
+
+  // Your Domain name with URL path or IP address with path
+  https.begin(*client, "https://cony-home-automation-web.vercel.app/api/esp8266/auth");
+
+  https.addHeader("Content-Type", "application/json");
+
+  // JSON Declare
+  DynamicJsonDocument doc(2048);
+
+  doc["chip_id"] = String(ESP.getChipId());
+  doc["mac"] = WiFi.macAddress();
+  doc["api_key"] = "$argon2id$v=19$m=65536,t=3,p=4$gX6VFv3fKlvvqkvHBbpxWw$XVqT4tzrOjNj/oSoHx2qb5IG6TYB8mNM/SYdWQXHS+g";
+
+  // Serialize JSON document
+  String json;
+  serializeJson(doc, json);
+
+  // Send HTTP POST request
+  int httpResponseCode = https.POST(json);
+  if (httpResponseCode == 200)
+  {
+    String payload = https.getString();
+    StaticJsonDocument<512> jsonBuffer;
+    deserializeJson(doc, payload);
+    String newToken = doc["token"];
+    jsonWebToken = newToken;
+  }
+  https.end();
+  return jsonWebToken; // return the jwt token value only
 }
 
-String ConyHomeAutomation::fetch(String token){
+String ConyHomeAutomation::fetch(String token)
+{
   String obj;
-  if(WiFi.status() == WL_CONNECTED && (millis() - lastTime) > 3000 ){   
-    WiFiClient client;
-    HTTPClient http;
+  if (WiFi.status() == WL_CONNECTED && (millis() - lastTime) > 3000){
+    std::unique_ptr<BearSSL::WiFiClientSecure>client(new BearSSL::WiFiClientSecure);
+
+    client->setFingerprint(fingerprint);
+    
+    HTTPClient https;
 
     // Your Domain name with URL path or IP address with path
-    http.begin(client, "http://192.168.100.2:8080/api/fetch");
+    https.begin(*client, "https://cony-home-automation-web.vercel.app/api/fetch");
 
-    http.addHeader("Content-Type", "application/json");
+    https.addHeader("Content-Type", "application/json");
 
     // JSON Declare
     DynamicJsonDocument doc(2048);
-    doc["api_key"] = "$argon2id$v=19$m=65536,t=3,p=4$gX6VFv3fKlvvqkvHBbpxWw$XVqT4tzrOjNj/oSoHx2qb5IG6TYB8mNM/SYdWQXHS+g";;
+    doc["api_key"] = "$argon2id$v=19$m=65536,t=3,p=4$gX6VFv3fKlvvqkvHBbpxWw$XVqT4tzrOjNj/oSoHx2qb5IG6TYB8mNM/SYdWQXHS+g";
+    ;
     doc["token"] = token;
     doc["chip_id"] = String(ESP.getChipId());
     doc["mac"] = WiFi.macAddress();
 
-        // Serialize JSON document
+    // Serialize JSON document
     String json;
     serializeJson(doc, json);
-      
+
     // Send HTTP POST request
-    int httpResponseCode = http.POST(json);
-    
-    if (httpResponseCode == 200) {
-      String payload = http.getString();
+    int httpResponseCode = https.POST(json);
 
-      StaticJsonDocument<512> jsonBuffer;
-
+    if (httpResponseCode == 200)
+    {
+      String payload = https.getString();
       obj = payload;
     }
-    else {
+    else
+    {
       Serial.print("Error code: ");
       Serial.println(httpResponseCode);
       Serial.println("Fetching error");
     }
-    
+
     lastTime = millis();
-  }else{
-    if((millis() - lastTime) > 3000){
+  }
+  else
+  {
+    if ((millis() - lastTime) > 3000)
+    {
       lastTime = millis();
     }
     return {};
@@ -123,26 +145,30 @@ String ConyHomeAutomation::fetch(String token){
  first parameter is EEPROM address to store the value
  second parameter is value tp be stored.
 */
-void ConyHomeAutomation::writeStringIntoEEPROM(int address, String str){
+void ConyHomeAutomation::writeStringIntoEEPROM(int address, String str)
+{
   byte len = str.length();
   EEPROM.write(address, len);
 
-  for (int i = 0; i < len; i++){
+  for (int i = 0; i < len; i++)
+  {
     EEPROM.write(address + 1 + i, str[i]);
   }
-  EEPROM.commit();  
+  EEPROM.commit();
 }
 
 /*
  This function allow to get a value from EEPROM memory
  first parameter is EEPROM address of previously stored value
 */
-String ConyHomeAutomation::readStringFromEEPROM(int address){
+String ConyHomeAutomation::readStringFromEEPROM(int address)
+{
   int len = EEPROM.read(address);
   char data[len + 1];
 
-  for (int i = 0; i < len; i++){
-  data[i] = EEPROM.read(address + 1 + i);
+  for (int i = 0; i < len; i++)
+  {
+    data[i] = EEPROM.read(address + 1 + i);
   }
   data[len] = '\0';
 
